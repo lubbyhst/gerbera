863e9c72ce53da47b74b280051eb5d1162efcd64
lubbyhst <lubby-hst@gmx.net>
2018-11-07 14:05:47 +0100

    Update install-libupnp-rpi-zero.sh

--- scripts/install-libupnp-rpi-zero.sh
+++ scripts/install-libupnp-rpi-zero.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
-
-
+#!/bin/sh
+if ! [ "$(id -u)" = 0 ]; then
+    echo "Please run this script with superuser access!"
+    exit 1
+fi
+set -ex
+# other dependencies may needed.
 # do the manual way because everything else are a pain in the ass
 
 wget http://ftp.de.debian.org/debian/pool/main/p/pupnp-1.8/libupnp-dev_1.8.4-2_armhf.deb
